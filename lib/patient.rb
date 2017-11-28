d8acefbc4de531460304890f36105a14680e5049
Steven Senkowski <scsenkowski@gmail.com>
2017-11-28 16:25:48 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,6 +15,3 @@ class Patient
   end
 
 end
-
-Patient #add_appointment takes in an argument of an apppointment and adds that appointment to it's list of appointments and tells that appointmen
-t it belongs to the patient
