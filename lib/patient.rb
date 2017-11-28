1666b244513f40671eea3e901be4be256ad45300
Steven Senkowski <scsenkowski@gmail.com>
2017-11-28 16:25:28 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,6 +8,13 @@ class Patient
 
   def add_appointment(appointment)
     @appointments << appointment
+      @appontments.collect do |appointment|
+        appointment.doctor
+      end
+    end
   end
 
 end
+
+Patient #add_appointment takes in an argument of an apppointment and adds that appointment to it's list of appointments and tells that appointmen
+t it belongs to the patient
